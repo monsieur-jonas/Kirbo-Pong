e6d2b39f80baf2d527fdbbbc2df59dbc1fd4b25d
monsieur-jonas <72392671+monsieur-jonas@users.noreply.github.com>
2020-12-17 00:35:31 +0100

    Add files via upload

---
+++ js/Perso.js
@@ -0,0 +1,33 @@
+class Perso extends ElementHtml{
+    constructor($html) {
+        super($html);
+        this.$bulle=$html.find(".bulle");
+        this.mots=[
+            "Poyo?"
+            ,"P O Y O !"
+            ,"PoYo ?"
+            ,"..."
+            ,"Oyop"
+            ,"PoyooooooOOOOOO ?"
+            ,"Mhmm poyo"
+            ,"Kouisine"
+            ,"POYOOOO"
+        ];
+    }
+    parle(blabla){
+        let ici=this;
+        this.$bulle.text(blabla);
+        setTimeout(function(){
+            ici.$bulle.text("");
+        },1500);
+    }
+
+    /**
+     * Renvoie un mot aléatoire
+     * @returns {string}
+     */
+    get motAleatoire(){
+        return this.mots[Math.floor(Math.random() * this.mots.length)];
+    }
+
+}
